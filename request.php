110ee95a98159b43d96af1960db2a743a922050c
gizmo876 <theshvk@gmail.com>
2016-11-11 21:53:10 -0500

    ex3

--- request.php
+++ request.php
@@ -2,6 +2,7 @@
 // accept a term (keyword)
 // respond with a value
 $query = $_GET['q'];
+$all = $_GET['all'];
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "A place that sells alcholic beverages",
@@ -11,5 +12,19 @@ $definition = [
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+if($all==true and $query==null){
+    header("Content-type: text/xml");
+    
+    $str = '<entries>'; //xml - open tag
+    foreach($definitions as $key=>$value){
+        $str.='<definition name = \''.$key.'\'>'.$value.'</definition>';
+    }
+     $str.='</entries>'; //xml - close tag
+      $xml = new SimpleXMLElement($str);
+    echo $xml->asXML();
+}
+else{
+	print "<h3>" . strtoupper($query) . "</h3>";
+	print "<p>" . $definition[$query] . "</p>";
+}
+?>
